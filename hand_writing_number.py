90c5a841a86c5c9e1f22159ae39ea09396b6366a
Wilson <wangjinlong98889@gmail.com>
2018-06-11 13:14:26 -0500

    Add files via upload

---
+++ hand_writing_number.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+#read image  and convert color
+img = cv2.imread('digits.png')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#divide the image to 5000 pieces, each of which has 20x20 
+cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
+
+#convert to numpy arrays 
+x = np.array(cells)
+
+#half data for training and half to be tested 
+train = x[:,:50].reshape(-1,400).astype(np.float32)
+test = x[:,50:100].reshape(-1,400).astype(np.float32)
+
+#create tag of train and test 
+k = np.arange(10)
+train_labels = np.repeat(k,250)[:,np.newaxis]
+test_labels = train_labels.copy()
+
+#create a K-Nearest Neighbour classifer and train data then test  
+knn = cv2.ml.KNearest_create()
+knn.train(train,cv2.ml.ROW_SAMPLE,train_labels)
+ret,result,neighbours,dist = knn.findNearest(test,k=5)
+
+#check the accuracy  
+matches = result == test_labels
+correct = np.count_nonzero(matches)
+accuracy = correct*100.0 / result.size
+print(accuracy)
